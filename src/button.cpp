02ccf06b7bfe6cee52303fdc2c3ae5a0fe2f79a5
David Harris <charrisdavid@gmail.com>
2026-07-07 21:06:34 -0500

    Add button debounce logic

--- src/button.cpp
+++ src/button.cpp
@@ -6,7 +6,13 @@ namespace
 // private variables
 {
      const uint8_t BUTTON_PIN = 2;
+     const unsigned long DEBOUNCE_TIME_MS = 50;
+
+
     bool isPressed = false;
+    bool lastRawState = false;
+
+    unsigned long lastChangeTime = 0;
 }
 
 //public functions
@@ -19,8 +25,20 @@ namespace
     void Button::Update()
     {
       // Implementation for button update
-      isPressed = digitalRead(BUTTON_PIN) == LOW;
+      bool rawState = digitalRead(BUTTON_PIN) == LOW;
+      unsigned long currentTime = millis();
+     
+    if (rawState != lastRawState)
+    {
+        lastChangeTime = currentTime;
+        lastRawState = rawState;
     }
+
+    if (currentTime - lastChangeTime >= DEBOUNCE_TIME_MS)
+    {
+        isPressed = rawState;
+    }
+}
 
     bool Button::IsPressed()
     {
